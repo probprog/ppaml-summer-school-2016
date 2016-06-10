880053c0a84c921d026067c07a3dcca204b46feb
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2016-06-10 19:04:29 +0100

    updated slides for intro to functional programming

--- lectures/intro-to-functional-programming/snippets/looping.clj
+++ lectures/intro-to-functional-programming/snippets/looping.clj
@@ -9,9 +9,8 @@
 ; => ArithmeticException integer overflow  
 ;    clojure.lang.Numbers.throwIntOverflow (Numbers.java:1501)
 
-(defn factorial 
+(defn factorial [n]
   "computes n * (n-1) * ... * 1"
-  [n]
   (if (= n 1)
     1N
     (* n (factorial (- n 1)))))
@@ -23,6 +22,9 @@
     1
     (*' n (factorial (- n 1)))))
 
+
+(let [[a b & others] stuff])
+
 (factorial 10000)
 ; => StackOverflowError   clojure.lang.Numbers.equal (Numbers.java:216)
 
@@ -31,24 +33,70 @@
   (loop [result 1
          ivals (range 2 (+ n 1))]
     (if (seq ivals)
-      (recur (* result (first ivals))
+      (recur (*' result (first ivals))
              (rest ivals))
       result)))
 
-(defn factorial [n]
-  "computes n * (n-1) * ... * 1"
-  (reduce ;; inputs: current result and next value 
-          (fn [result i]
-            ;; output: result
-            (* result i))
-          1
-          (range (+ n 1))))
+(factorial 10000)
+; => 4023872600770937735437024339230039857193748642107146325437999104
+;    2993851239862902059204420848696940480047998861019719605863166687
+;    2994808558901323829669944590997424504087073759918823627727188732
+;    5197795059509952761208749754624970436014182780946464962910563938
+;    ...
+
+
+(defn floop 
+  "inner loop for factorial"
+  [result ivals]
+  (if (seq ivals)
+    (floop (*' result (first ivals))
+           (rest ivals))
+    result))
 
 (defn factorial [n]
   "computes n * (n-1) * ... * 1"
-  (reduce * 1 (range 2 (+ n 1))))
+  (floop 1 
+         (range 2 (+ n 1))))
+
+(defn floop 
+  "inner loop for factorial"
+  [result ivals]
+  (if (seq ivals)
+    (recur (*' result (first ivals))
+           (rest ivals))
+    result))
+
+(defn factorial [n]
+  "computes n * (n-1) * ... * 1"
+  (apply (fn floop [result ivals]
+           (if (seq ivals)
+             (recur (*' result (first ivals))
+                    (rest ivals))
+             result))
+         [1 (range 2 (+ n 1))]))
+
+(apply + [1 2])
+; => 3
+
+(defn factorial [n]
+  "computes n * (n-1) * ... * 1"
+  (reduce (fn [result i]
+            (*' result i))
+          1 (range 2 (+ n 1))))
+
+(defn factorial [n]
+  "computes n * (n-1) * ... * 1"
+  (reduce *' 1 (range 2 (+ n 1))))
 
 
 (defn factorial [n]
   "computes n * (n-1) * ... * 1"
-  (reduce * (range 1 (+ n 1))))
+  (reduce *' (range 1 (+ n 1))))
+
+(defn cumsum [values]
+  "returns a vector containing the 
+  cumulative sum of values in a collection."
+  (reduce (fn [output v]
+            (conj output 
+                  (+ (peek output) v)))
+          [] values))
