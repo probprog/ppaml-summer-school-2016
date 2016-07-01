bb1b60f42021edbff9b7c203a03daf4e9144b96a
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2016-07-01 10:21:06 +0100

    added anglican and gorilla repl to project dependencies for exercises

--- lectures/intro-to-functional-programming/exercises/project.clj
+++ lectures/intro-to-functional-programming/exercises/project.clj
@@ -3,7 +3,9 @@
   :url "https://bitbucket.org/probprog/ppaml-summer-school-2016/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [anglican "0.9.0"]
+                 [lein-gorilla "0.3.6"]]
   :main ^:skip-aot examples.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
