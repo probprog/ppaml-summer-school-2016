7583cdc3515658fb03ee74abc4437c720e4d748c
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2016-07-02 17:46:52 +0100

    move gorilla repl from deps to plugins

--- exercises/project.clj
+++ exercises/project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [anglican "0.9.0"]
-                 [lein-gorilla "0.3.6"]]
+                 [anglican "0.9.0"]]
+  :plugins [[lein-gorilla "0.3.6"]]
   :main ^:skip-aot examples.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
