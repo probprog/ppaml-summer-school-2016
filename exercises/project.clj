673d186b00c678621d0bb2ddf12375ccfd4570a2
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2016-07-20 11:23:26 +0100

    updated project.clj anglican to 1.0.0-SNAPSHOT

--- exercises/project.clj
+++ exercises/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-gorilla "0.3.6"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [anglican "0.10.0-PRERELEASE"]
+                 [anglican "1.0.0-SNAPSHOT"]
                  [net.mikera/core.matrix "0.52.2"]
                  [net.mikera/vectorz-clj "0.44.1"]
                  [net.polyc0l0r/clj-hdf5 "0.2.2-SNAPSHOT"]]
